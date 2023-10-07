77eedfa00fc8886c20590d8c9540a1c3827ea02c
Pedro Spengler <117318841+PedriSpengler@users.noreply.github.com>
2023-10-06 23:27:47 -0400

    Create aula1.go
    # Challenge: 
    
    By the time you calculate the answer to the Elves' question, they've already realized that the Elf carrying the most Calories of food might eventually run out of snacks.
    
    To avoid this unacceptable situation, the Elves would instead like to know the total Calories carried by the top three Elves carrying the most Calories. That way, even if one of those Elves runs out of snacks, they still have two backups.
    
    In the example above, the top three Elves are the fourth Elf (with 24000 Calories), then the third Elf (with 11000 Calories), then the fifth Elf (with 10000 Calories). The sum of the Calories carried by these three elves is 45000.
    
    Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?

---
+++ aula1.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main(){
+
+	file, err := os.Open("./calorias.txt")
+	if err != nil {
+		fmt.Println("Erro ao abrir o arquivo:", err)
+		return
+	}
+	
+	defer file.Close()
+
+	// Cria um leitor de arquivo linha por linha
+	scanner := bufio.NewScanner(file)
+
+	// Leitura e impressão do arquivo .txt
+	var elfo1, elfo2, elfo3 int
+	var fulanoElfos int = 1
+	var maior1 int = 0
+	var maior2 int = 0
+	var maior3 int = 0
+	var soma, somaCalElfos int
+	for scanner.Scan(){
+		line := scanner.Text()
+		if line == "" {
+			soma = 0
+			fulanoElfos++
+		} else{
+			num, err := strconv.Atoi(line)
+			if err != nil {
+				fmt.Println("Erro ao converter linha em número:", err)
+				continue
+			}
+			soma += num 
+			if soma > maior3 { // soma = 2000
+				maior3 = soma // maior3 = 2000 
+				elfo3 = fulanoElfos // elfo3 = 54
+				if maior3 > maior2 { // 
+					maior3, maior2 = maior2, maior3 // maior3 = 0 ; maior2 = 2000
+					elfo3, elfo2 = elfo2, elfo3 // elfo3 = 0; elfo2 = 54
+					if maior2 > maior1 { //
+						maior2, maior1 = maior1, maior2 // maior2 = 0 ; maior1 = 2000
+						elfo2, elfo1 = elfo1, elfo2 // elfo2 = 0 ; elfo1 = 54
+					}
+				}
+			}
+		}	
+	}
+	somaCalElfos = maior1 + maior2 + maior3
+	fmt.Printf("TOP3 elfos em ordem: %d:%d, %d:%d, %d:%d\n", elfo1, maior1, elfo2, maior2, elfo3, maior3)
+	fmt.Printf("Soma dos TOP3 elfo: %d", somaCalElfos)
+}
